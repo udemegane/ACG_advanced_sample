2680dd2b86b6c30ba8d46e54af50ae16876767cd
udemegane <cappuccino1204.ck@gmail.com>
2021-11-26 21:08:00 +0900

    lint後

--- src/acgCustomRenderingPipeline.ts
+++ src/acgCustomRenderingPipeline.ts
@@ -1,10 +1,22 @@
-import { Camera, Engine, EngineStore, PostProcessRenderPipeline, Scene } from "@babylonjs/core";
+import {
+  Camera,
+  Engine,
+  EngineStore,
+  PostProcessRenderPipeline,
+  Scene,,
+} from "@babylonjs/core";
 
 export class ACGCustomRenderingPipeline extends PostProcessRenderPipeline {
-    private _scene: Scene;
-    private _camerasToBeAttached: Array<Camera> = [];
-    constructor(name: string = "", scene: Scene = EngineStore.LastCreatedScene!, cameras?: Camera[]) {
-        super(scene.getEngine(), name);
-        scene.enableGeometryBufferRenderer();
-    }
+  private _scene: Scene;
+
+  private _camerasToBeAttached: Array<Camera> = [];
+
+  constructor(
+    name = "",
+    scene: Scene = EngineStore.LastCreatedScene!,
+    cameras?: Camera[],
+  ) {
+    super(scene.getEngine(), name);
+    scene.enableGeometryBufferRenderer();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,57 @@
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-import "@babylonjs/loaders/glTF";
-import { ACGCustomRenderingPipeline } from "./acgCustomRenderingPipeline"
+import '@babylonjs/core/Debug/debugLayer';
+import '@babylonjs/inspector';
+import '@babylonjs/loaders/glTF';
 import {
-    Engine,
-    Scene,
-    ArcRotateCamera,
-    Vector3,
-    HemisphericLight,
-    Mesh,
-    MeshBuilder,
-    PostProcessRenderPipelineManager
-} from "@babylonjs/core";
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  PostProcessRenderPipelineManager
+} from '@babylonjs/core';
+import { ACGCustomRenderingPipeline } from './acgCustomRenderingPipeline';
 
 class Main {
-    constructor() {
-        const canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "appCanvas";
-        document.body.appendChild(canvas);
+  constructor() {
+    const canvas = document.createElement('canvas');
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.id = 'appCanvas';
+    document.body.appendChild(canvas);
 
-        const engine: Engine = new Engine(canvas, true);
-        const scene: Scene = new Scene(engine);
+    const engine: Engine = new Engine(canvas, true);
+    const scene: Scene = new Scene(engine);
 
-        const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2.0, Math.PI / 2.0, 2.0, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        const light1: HemisphericLight = new HemisphericLight("Light1", new Vector3(1, 1, 0), scene);
-        const sphere: Mesh = MeshBuilder.CreateSphere("Sphere", { diameter: 1 }, scene);
+    const camera: ArcRotateCamera = new ArcRotateCamera(
+      'Camera',
+      Math.PI / 2.0,
+      Math.PI / 2.0,
+      2.0,
+      Vector3.Zero(),
+      scene
+    );
+    camera.attachControl(canvas, true);
+    const light1: HemisphericLight = new HemisphericLight(
+      'Light1',
+      new Vector3(1, 1, 0),
+      scene
+    );
+    const sphere: Mesh = MeshBuilder.CreateSphere(
+      'Sphere',
+      { diameter: 1 },
+      scene
+    );
 
-        const pipeline = new ACGCustomRenderingPipeline("renderingPipeline", scene, [camera]);
+    const pipeline = new ACGCustomRenderingPipeline(
+      'renderingPipeline',
+      scene,
+      [camera]
+    );
 
-        engine.runRenderLoop(() => {
-            scene.render();
-        });
-    }
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  }
 }
